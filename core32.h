5b47e3c9e53ccf7bf5ab10221af31b3d56c10c2c
cancel <cancel@cancel.fm>
2022-02-03 15:28:39 +0900

    Remove unused typedef

--- core32.h
+++ core32.h
@@ -14,7 +14,6 @@ WITH REGARD TO THIS SOFTWARE.
 typedef unsigned char Uint8;
 typedef signed char Sint8;
 typedef unsigned short Uint16;
-typedef signed short Sint16;
 typedef unsigned int Uint32;
 
 typedef struct {
