3c6b789a21a4c96f66ea9d538fa6d0d1a2457130
cancel <cancel@cancel.fm>
2023-01-13 03:24:48 +0900

    Add new opcode implementation (WIP)

--- uxn_core.c
+++ uxn_core.c
@@ -30,10 +30,21 @@ WITH REGARD TO THIS SOFTWARE.
 #define DEVW(x, y) { if(bs) { u->deo(u, (x), (y) >> 8); u->deo(u, ((x) + 1) & 0xFF, (y)); } else u->deo(u, x, (y)); }
 #define WARP(x) { if(bs) pc = (x); else pc += (UxnI8)(x); }
 
+#define DISPATCH(opcode, body) \
+	case opcode|0x80|0x40|0x20: {enum{bs=1}; src = u->rst, dst = u->wst; kptr = src->ptr, sp = &kptr; body break;}\
+	case opcode|0x80|0x40|0x00: {enum{bs=0}; src = u->rst, dst = u->wst; kptr = src->ptr, sp = &kptr; body break;}\
+	case opcode|0x80|0x00|0x20: {enum{bs=1}; src = u->wst, dst = u->rst; kptr = src->ptr, sp = &kptr; body break;}\
+	case opcode|0x80|0x00|0x00: {enum{bs=0}; src = u->wst, dst = u->rst; kptr = src->ptr, sp = &kptr; body break;}\
+	case opcode|0x00|0x40|0x20: {enum{bs=1}; src = u->rst, dst = u->wst; sp = &src->ptr; body break;}\
+	case opcode|0x00|0x40|0x00: {enum{bs=0}; src = u->rst, dst = u->wst; sp = &src->ptr; body break;}\
+	case opcode|0x00|0x00|0x20: {enum{bs=1}; src = u->wst, dst = u->rst; sp = &src->ptr; body break;}\
+	case opcode|0x00|0x00|0x00: {enum{bs=0}; src = u->wst, dst = u->rst; sp = &src->ptr; body break;}\
+
+
 unsigned int
 UxnExec(UxnCore *u, unsigned int limit)
 {
-	unsigned int a, b, c, j, k, bs, instr, pc;
+	unsigned int a, b, c, j, k, instr, pc;
 	UxnU8 kptr, *sp;
 	UxnStack *src, *dst;
 	pc = u->pc;
@@ -41,60 +52,45 @@ UxnExec(UxnCore *u, unsigned int limit)
 		limit--;
 		instr = u->ram[pc];
 		pc = (pc + 1) & 0xFFFFu;
-		if (!instr) { u->fault_code = 1; goto done; }
-		/* Return Mode */
-		if(instr & 0x40) {
-			src = u->rst; dst = u->wst;
-		} else {
-			src = u->wst; dst = u->rst;
-		}
-		/* Keep Mode */
-		if(instr & 0x80) {
-			kptr = src->ptr;
-			sp = &kptr;
-		} else {
-			sp = &src->ptr;
-		}
-		/* Short Mode */
-		bs = instr & 0x20 ? 1 : 0;
-		switch(instr & 0x1F) {
-		/* Stack */
-		case 0x00: /* LIT */ if(bs) { PEEK16(a, pc) PUSH16(src, a) pc += 2; }
-		                     else   { a = u->ram[pc]; PUSH8(src, a) pc++; } break;
-		case 0x01: /* INC */ POP(a) PUSH(src, a + 1) break;
-		case 0x02: /* POP */ POP(a) break;
-		case 0x03: /* NIP */ POP(a) POP(b) PUSH(src, a) break;
-		case 0x04: /* SWP */ POP(a) POP(b) PUSH(src, a) PUSH(src, b) break;
-		case 0x05: /* ROT */ POP(a) POP(b) POP(c) PUSH(src, b) PUSH(src, a) PUSH(src, c) break;
-		case 0x06: /* DUP */ POP(a) PUSH(src, a) PUSH(src, a) break;
-		case 0x07: /* OVR */ POP(a) POP(b) PUSH(src, b) PUSH(src, a) PUSH(src, b) break;
-		/* Logic */
-		case 0x08: /* EQU */ POP(a) POP(b) PUSH8(src, b == a) break;
-		case 0x09: /* NEQ */ POP(a) POP(b) PUSH8(src, b != a) break;
-		case 0x0A: /* GTH */ POP(a) POP(b) PUSH8(src, b > a) break;
-		case 0x0B: /* LTH */ POP(a) POP(b) PUSH8(src, b < a) break;
-		case 0x0C: /* JMP */ POP(a) WARP(a) break;
-		case 0x0D: /* JCN */ POP(a) POP8(b) if(b) WARP(a) break;
-		case 0x0E: /* JSR */ POP(a) PUSH16(dst, pc) WARP(a) break;
-		case 0x0F: /* STH */ POP(a) PUSH(dst, a) break;
-		/* Memory */
-		case 0x10: /* LDZ */ POP8(a) PEEK(b, a) PUSH(src, b) break;
-		case 0x11: /* STZ */ POP8(a) POP(b) POKE(a, b) break;
-		case 0x12: /* LDR */ POP8(a) PEEK(b, pc + (UxnI8)a) PUSH(src, b) break;
-		case 0x13: /* STR */ POP8(a) POP(b) c = pc + (UxnI8)a; POKE(c, b) break;
-		case 0x14: /* LDA */ POP16(a) PEEK(b, a) PUSH(src, b) break;
-		case 0x15: /* STA */ POP16(a) POP(b) POKE(a, b) break;
-		case 0x16: /* DEI */ POP8(a) DEVR(b, a) PUSH(src, b) break;
-		case 0x17: /* DEO */ POP8(a) POP(b) DEVW(a, b) if (u->fault_code) goto done; break;
-		/* Arithmetic */
-		case 0x18: /* ADD */ POP(a) POP(b) PUSH(src, b + a) break;
-		case 0x19: /* SUB */ POP(a) POP(b) PUSH(src, b - a) break;
-		case 0x1A: /* MUL */ POP(a) POP(b) PUSH(src, (unsigned int)b * a) break;
-		case 0x1B: /* DIV */ POP(a) POP(b) if(a == 0) { u->fault_code = 4; goto done; } PUSH(src, b / a) break;
-		case 0x1C: /* AND */ POP(a) POP(b) PUSH(src, b & a) break;
-		case 0x1D: /* ORA */ POP(a) POP(b) PUSH(src, b | a) break;
-		case 0x1E: /* EOR */ POP(a) POP(b) PUSH(src, b ^ a) break;
-		case 0x1F: /* SFT */ POP8(a) POP(b) c = b >> (a & 0x0F) << ((a & 0xF0) >> 4); PUSH(src, c) break;
+		if (!instr) { u->fault_code = 1; goto done; } // TODO
+		switch(instr) {
+		// /* BRK */ case 0x00: u->fault_code = 1; goto done;
+		DISPATCH(0x00, if(bs) { PEEK16(a, pc) PUSH16(src, a) pc += 2; } else { a = u->ram[pc]; PUSH8(src, a) pc++; } )
+	/* Stack */
+		/* INC */ DISPATCH(0x01, POP(a) PUSH(src, a + 1) )
+		/* POP */ DISPATCH(0x02, POP(a) )
+		/* NIP */ DISPATCH(0x03, POP(a) POP(b) PUSH(src, a) )
+		/* SWP */ DISPATCH(0x04, POP(a) POP(b) PUSH(src, a) PUSH(src, b) )
+		/* ROT */ DISPATCH(0x05, POP(a) POP(b) POP(c) PUSH(src, b) PUSH(src, a) PUSH(src, c) )
+		/* DUP */ DISPATCH(0x06, POP(a) PUSH(src, a) PUSH(src, a) )
+		/* OVR */ DISPATCH(0x07, POP(a) POP(b) PUSH(src, b) PUSH(src, a) PUSH(src, b) )
+	/* Logic */
+		/* EQU */ DISPATCH(0x08, POP(a) POP(b) PUSH8(src, b == a) )
+		/* NEQ */ DISPATCH(0x09, POP(a) POP(b) PUSH8(src, b != a) )
+		/* GTH */ DISPATCH(0x0A, POP(a) POP(b) PUSH8(src, b > a) )
+		/* LTH */ DISPATCH(0x0B, POP(a) POP(b) PUSH8(src, b < a) )
+		/* JMP */ DISPATCH(0x0C, POP(a) WARP(a) )
+		/* JCN */ DISPATCH(0x0D, POP(a) POP8(b) if(b) WARP(a) )
+		/* JSR */ DISPATCH(0x0E, POP(a) PUSH16(dst, pc) WARP(a) )
+		/* STH */ DISPATCH(0x0F, POP(a) PUSH(dst, a) )
+	/* Memory */
+		/* LDZ */ DISPATCH(0x10, POP8(a) PEEK(b, a) PUSH(src, b) )
+		/* STZ */ DISPATCH(0x11, POP8(a) POP(b) POKE(a, b) )
+		/* LDR */ DISPATCH(0x12, POP8(a) PEEK(b, pc + (UxnI8)a) PUSH(src, b) )
+		/* STR */ DISPATCH(0x13, POP8(a) POP(b) c = pc + (UxnI8)a; POKE(c, b) )
+		/* LDA */ DISPATCH(0x14, POP16(a) PEEK(b, a) PUSH(src, b) )
+		/* STA */ DISPATCH(0x15, POP16(a) POP(b) POKE(a, b) )
+		/* DEI */ DISPATCH(0x16, POP8(a) DEVR(b, a) PUSH(src, b) )
+		/* DEO */ DISPATCH(0x17, POP8(a) POP(b) DEVW(a, b) if (u->fault_code) goto done; )
+	/* Arithmetic */
+		/* ADD */ DISPATCH(0x18, POP(a) POP(b) PUSH(src, b + a) )
+		/* SUB */ DISPATCH(0x19, POP(a) POP(b) PUSH(src, b - a) )
+		/* MUL */ DISPATCH(0x1A, POP(a) POP(b) PUSH(src, (unsigned int)b * a) )
+		/* DIV */ DISPATCH(0x1B, POP(a) POP(b) if(a == 0) { u->fault_code = 4; goto done; } PUSH(src, b / a) )
+		/* AND */ DISPATCH(0x1C, POP(a) POP(b) PUSH(src, b & a) )
+		/* ORA */ DISPATCH(0x1D, POP(a) POP(b) PUSH(src, b | a) )
+		/* EOR */ DISPATCH(0x1E, POP(a) POP(b) PUSH(src, b ^ a) )
+		/* SFT */ DISPATCH(0x1F, POP8(a) POP(b) c = b >> (a & 0x0F) << ((a & 0xF0) >> 4); PUSH(src, c) )
 		}
 	}
 done:
