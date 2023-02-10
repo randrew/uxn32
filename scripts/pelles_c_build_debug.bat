11a0a0cae993b3be8c4065b61307fc5e87892722
cancel <cancel@cancel.fm>
2023-02-10 12:34:20 +0900

    Add Pelles C build scripts (test)

---
+++ scripts/pelles_c_build_debug.bat
@@ -0,0 +1,14 @@
+@echo off
+if not exist uxn32.c echo Run this with the uxn32 root source directory as your working directory (not from within scripts) & pause & exit /b 1
+@echo on
+
+if not exist build mkdir build
+
+set INCLUDE=C:\Program Files\PellesC\Include\Win;C:\Program Files\PellesC\Include
+set LIB=C:\Program Files\PellesC\Lib\Win;C:\Program Files\PellesC\Lib
+
+"C:\Program Files\PellesC\Bin\pocc.exe" -std:C99 -Tx86-coff -Zi -MT -Ob1 -fp:precise -W0 -Gz -Ze -D_DEBUG -DWIN32 -D_WINDOWS -D_MBCS -D_WIN32_WINNT=0x0400 "uxn_core.c" -Fo"build\uxn_core.obj"
+"C:\Program Files\PellesC\Bin\pocc.exe" -std:C99 -Tx86-coff -Zi -MT -Ob1 -fp:precise -W0 -Gz -Ze -D_DEBUG -DWIN32 -D_WINDOWS -D_MBCS -D_WIN32_WINNT=0x0400 "uxn_lz.c" -Fo"build\uxn_lz.obj"
+"C:\Program Files\PellesC\Bin\pocc.exe" -std:C99 -Tx86-coff -Zi -MT -Ob1 -fp:precise -W0 -Gz -Ze -D_DEBUG -DWIN32 -D_WINDOWS -D_MBCS -D_WIN32_WINNT=0x0400 "uxn32.c" -Fo"build\uxn32.obj"
+"C:\Program Files\PellesC\Bin\porc.exe" -L0x409 -D_DEBUG "uxn32.rc" -Fo"build\uxn32.res"
+"C:\Program Files\PellesC\Bin\polink.exe" -debug -debugtype:cv -subsystem:windows -machine:x86 user32.lib gdi32.lib shell32.lib shlwapi.lib comdlg32.lib comctl32.lib winmm.lib -out:"build\Uxn32.exe" "build\uxn_core.obj" "build\uxn_lz.obj" "build\uxn32.obj" "build\uxn32.res"
