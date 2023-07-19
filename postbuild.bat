7b11ed892e67663510659accc106aed6692ec208
easrng <me@easrng.net>
2023-07-19 11:55:36 -0400

    remove postbuild

--- postbuild.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x86\mt.exe" -nologo -manifest ".\uxn32.exe.manifest" -outputresource:"C:\uxn32\Release/uxn32.exe;#1" 2>nul
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x86\mt.exe" -nologo -manifest ".\uxn32.exe.manifest" -outputresource:"C:\uxn32\Debug/uxn32.exe;#1" 2>nul
